77e5080e8913640a6d39d2b5ffdd9eca0de6d993
sabhilash <sabhilash@fas.harvard.edu>
2016-03-09 06:07:07 -0500

    Update main.js
    Moved the controller code from webservice.js to main. Changed localhost to the hosted URL server: 104.236.250.11

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the helmeditor2App
  */
 angular.module('helmeditor2App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+  var baseUrl = 'http://104.236.250.11:8080/WebService/service/Sequence/';
+	var functUrl = baseUrl + $("#type").val() + '/';
+
+	$scope.getResult = function() {
+	   	var fullUrl = functUrl + $scope.input;
+	  	console.log(fullUrl);
+
+	  	$http.get(fullUrl)
+	     .then(function(response) {
+	     	console.log(response.data);
+	        $scope.result = response.data;
+	    });
+  	}
   });
