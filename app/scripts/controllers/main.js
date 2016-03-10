3f7e7dd98fa515e43bf5db1cdeef40f862021f23
Simili Abhilash <sabhilash@fas.harvard.edu>
2016-03-09 19:30:18 -0500

    Update main.js
    Included Factory for the webservice functionality. This factory can be accessed from various controllers within the application. 
    Also added a prototype code inside MainCntrl  to invoke the factory function to get HELM notation and HEM image.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,31 +8,106 @@
  * Controller of the helmeditor2App
  */
  
-angular.module('helmeditor2App')
-  .controller('MainCtrl', function ($scope, $http) {
+var app = angular.module('helmeditor2App');
+
+app.controller('MainCtrl', ['$scope', 'webServiceFactory', function ($scope, webServiceFactory) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    $scope.monomerTypes = [
-          { value: 'PEPTIDE', label:'PEPTIDE' },
-          { value: 'RNA', label:'RNA/DNA' }
+    /* Variables related to the prototype code in HTML */
+    $scope.polyTypes = [
+      { value: 'PEPTIDE', label:'PEPTIDE' },
+      { value: 'RNA', label:'RNA/DNA' }
     ];
-    $scope.seletedOption = $scope.monomerTypes[0];
+    $scope.polymerType = $scope.polyTypes[0];
+    $scope.status;
+    $scope.result;
+    
+    /* Event processing prototype - invoke factory function to get HELM notation */   
+    $scope.getHelmNotation = function (polymerType, inputSequence) {
+      webServiceFactory.getHelmNotation(polymerType.value, inputSequence)
+        .success(function (response) {
+            $scope.status = 'Success';
+            $scope.result = response;
+            console.log(response);
+        })
+        .error(function (error) {
+            $scope.status = 'Error: ' + error.message;
+        });
+    };
 
-    var baseUrl = 'http://104.236.250.11:8080/WebService/service/Sequence/';
-	
-	$scope.getResult = function() {
-	   	var functUrl = baseUrl + $scope.seletedOption.value + '/';
-		var fullUrl = functUrl + $scope.input;
-	  	console.log(fullUrl);
+    /* Event processing prototype - invoke factory function to get HELM image */ 
+    $scope.getHelmImage = function (inputSequence) {
+        webServiceFactory.getHelmImage(inputSequence)
+          .success(function (response) {
+              $scope.status = 'Success';
+              $scope.result = response; //TODO: Identify image processing logic 
+          })
+          .error(function (error) {
+              $scope.status = 'Error: ' + error.message;
+          });
+    };
+}]);
 
-	  	$http.get(fullUrl)
-	     .then(function(response) {
-	     	console.log(response.data);
-	        $scope.result = response.data;
-	    });
-  	}
-  });
+/*  Factory for webservice functionality */
+app.factory('webServiceFactory', ['$http', function($http) {
+    var baseUrl = 'http://104.236.250.11:8080/WebService/service/';
+    var webServiceFactory = {};
+
+    webServiceFactory.getHelmNotation = function (polymerType, inputSequence) {
+      var fullUrl = baseUrl + 'Sequence/' + polymerType + '/' + inputSequence;
+      console.log(fullUrl);
+      return $http.get(fullUrl);
+    };
+
+    webServiceFactory.getMonomerImage = function (monomerId, polymerType, showRgroups) {
+      var fullUrl = baseUrl + 'Image/Monomer/';
+      return $http.get(fullUrl, 
+        {params:{"monomerId": monomerId, "polymerType": polymerType, "showRgroups": showRgroups}});
+    };
+
+    webServiceFactory.getHelmImage = function (inputSequence) {
+      var fullUrl = baseUrl + 'Image/HELM/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+
+    webServiceFactory.getMolecularWeight = function (inputSequence) {
+      var fullUrl = baseUrl + 'Calculation/MolecularWeight/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+
+    webServiceFactory.getMolecularFormula = function (inputSequence) {
+      var fullUrl = baseUrl + 'Calculation/MolecularFormula/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+
+    webServiceFactory.getExtinctionCoefficient= function (inputSequence) {
+      var fullUrl = baseUrl + 'Calculation/ExtinctionCoefficient/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+
+    webServiceFactory.getConversionCanonical = function (inputSequence) {
+      var fullUrl = baseUrl + 'Conversion/Canonical/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+    
+    webServiceFactory.getConversionStandard = function (inputSequence) {
+      var fullUrl = baseUrl + 'Conversion/Standard/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+    
+    webServiceFactory.getConversionJson = function (inputSequence) {
+      var fullUrl = baseUrl + 'Conversion/JSON/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+    
+    webServiceFactory.getFastaProduce = function (inputSequence) {
+      var fullUrl = baseUrl + 'Fasta/Produce/' + inputSequence;
+      return $http.get(fullUrl);
+    };
+    
+    return webServiceFactory;
+}]);
