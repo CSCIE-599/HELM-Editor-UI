829a16517664e8ace90f518e52986d3e03067ff8
Justin Sanford <justin.h.sanford@gmail.com>
2016-04-05 10:37:18 -0700

    renamed helmcanvas_template.html to helmcanvas.html to match other templates

--- app/scripts/directives/helmcanvas.js
+++ app/scripts/directives/helmcanvas.js
@@ -10,7 +10,7 @@ angular.module('helmeditor2App')
   .directive('helmCanvas', function () {
     return {
       restrict: 'E',
-       templateUrl: 'templates/helmcanvas_template.html',
+       templateUrl: 'templates/helmcanvas.html',
        replace: true,
        scope: {
            graph: '=graph',
