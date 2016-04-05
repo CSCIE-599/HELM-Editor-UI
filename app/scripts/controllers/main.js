722a5002422043ec8f11492b54348f52425e7ea4
Justin Sanford <justin.h.sanford@gmail.com>
2016-04-05 13:49:56 -0700

    basic layout complete and initial integration with load sequence

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,10 +10,68 @@
 
 var app = angular.module('helmeditor2App');
 
-app.controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-});
+app.controller('MainCtrl', ['webService', function (webService) {
+	var main = this;
+	/* Toggle modal dialogue display */
+	main.modalShown = false;
+	main.toggleModal = function() {
+    this.modalShown = !main.modalShown;
+	};
+
+	/* Variables for loadsequence view */
+	main.polyTypes = [
+    { value: 'PEPTIDE', label:'PEPTIDE' },
+    { value: 'RNA', label:'RNA/DNA' },
+    { value: 'HELM', label:'HELM' },
+	];
+	main.polymerType = main.polyTypes[0];
+	main.result = '';
+
+	/* Check if need to validate HELM input, or convert input to Helm */
+	main.processInput = function (polymerType, inputSequence) {
+	  /* Check that input is not empty */
+    if (!angular.isDefined(inputSequence)) {
+      window.alert('Invalid input');
+      return;
+    }
+    /* TODO: Check that input is valid type? */
+    if (polymerType.value === 'HELM') {
+    	main.validateHelmNotation(inputSequence);
+    }
+    else {
+    	main.getHelmNotation(polymerType, inputSequence);
+    }
+    main.toggleModal();
+	};
+
+	/* Invoke factory function to get HELM notation */   
+	main.getHelmNotation = function (polymerType, inputSequence) {
+    var successCallback = function (helmNotation) {
+      main.result = helmNotation;
+    };
+    var errorCallback = function(response) {
+      main.result = response.data;
+      console.log(response.data);
+    };
+
+    switch(polymerType.value) {
+      case 'PEPTIDE':
+        webService.getHelmNotationPeptide(inputSequence).then(successCallback, errorCallback);
+        break;
+      case 'RNA':
+        webService.getHelmNotationRna(inputSequence).then(successCallback, errorCallback);
+    }
+  };
+
+  main.validateHelmNotation = function (inputSequence) {
+    var successCallback = function (helmNotation) {
+      main.result = helmNotation;
+    };
+    var errorCallback = function(response) {
+      main.result = response.data;
+      console.log(response.data);
+    };
+    
+    webService.validateHelmNotation(inputSequence).then(successCallback, errorCallback);
+  };
+}]);
