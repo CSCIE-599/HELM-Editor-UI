34a666dffe26b921c621fc24a49ee66cddd0e8f7
Justin Sanford <justin.h.sanford@gmail.com>
2016-04-17 18:27:13 +0200

    set up httpBackend to allow mocking of the database call

--- test/spec/services/monomerlibraryservice.js
+++ test/spec/services/monomerlibraryservice.js
@@ -3,15 +3,26 @@
 describe('Service: MonomerLibraryService', function () {
 
   // load the service's module
-  beforeEach(module('helmeditor2App'));
+  beforeEach(module('helmeditor2App.MonomerLibrary'));
 
   // instantiate service
-  var MonomerLibraryService;
-  beforeEach(inject(function (_MonomerLibraryService_) {
+  var MonomerLibraryService, $httpBackend;
+  beforeEach(inject(function (_MonomerLibraryService_, _$httpBackend_) {
     MonomerLibraryService = _MonomerLibraryService_;
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expect('GET', 'DefaultMonomerCategorizationTemplate.xml').respond('<x></x>');
+    $httpBackend.expect('GET', 'MonomerDBGZEncoded.xml').respond('<x></x>');
+    $httpBackend.flush();
   }));
 
+  // after each test, make sure to validate that there are no outstanding requests
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should do something', function () {
+    //console.log(MonomerLibraryService.getCategorizedDB());
     expect(!!MonomerLibraryService).toBe(true);
   });
 
