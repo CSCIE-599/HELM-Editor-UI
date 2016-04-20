4bd65f7882e873e507925d53be7c1ecc48a05cc1
Justin Sanford <justin.h.sanford@gmail.com>
2016-04-20 16:24:20 +0100

    cleaned up unnecessary functions

--- app/scripts/services/monomerlibraryservice.js
+++ app/scripts/services/monomerlibraryservice.js
@@ -225,46 +225,6 @@ angular.module('helmeditor2App.MonomerLibrary', ['cb.x2js'])
     self.getEncodedDB = getEncodedDB;
     // self.searchEncodedDB = searchEncodedDB;
 
-    // for each monomer in the parent group, it finds the corresponding monomer 
-    // from the encoded database and adds the info there to the categorized 
-    // database in order to help create one full database.
-//    function dbLinker(parent, polymerId) {
-//      var j;
-//      if(parent.Monomer !== undefined){
-//        var monomerList = parent.Monomer;
-//        for(j = 0; j < monomerList.length; j++){
-//          var encodedMonomer = getEncodedMonomer(monomerList[j]._name, polymerId);
-//          if(encodedMonomer === null){
-//            continue;
-//          }
-          /*
-          for(var prop in encodedMonomer){
-            monomerList[j].[prop] = encodedMonomer[prop];
-          }
-          */
-//          monomerList[j].encodedInfo = encodedMonomer;
-//          encodedMonomer.categorizedInfo = monomerList[j].encodedInfo;
-//        }
-//      }
-//      if(parent.MonomerGroup !== undefined){
-//        for(j = 0; j < parent.MonomerGroup.length; j++){
-//          dbLinker(parent.MonomerGroup[j], polymerId);
-//        }
-//      }
-//    }
-
-//    function initLink(){
-//      // 
-//      var polymerList = categorizedDB.Template.Polymer;
-//      for(var i = 0; i < polymerList.length; i++){
-//        //
-//        var polymerId = polymerList[i]._name;
-//        for(var j = 0; j < polymerList[i].MonomerGroup.length; j++){
-//          dbLinker(polymerList[i].MonomerGroup[j], polymerId);
-//        }
-//      }
-//    }
-/*
     // getPolymer, getMonomerGroup, and getMonomer all return
     // an object with a boolean returnSuccess property and optional
     // result property representing a type from the categorized database
@@ -307,63 +267,7 @@ angular.module('helmeditor2App.MonomerLibrary', ['cb.x2js'])
       }
       return output;
     }
-// /*
-//     var infoGrabber = function(nameList){
-//       //
-//       var i, j;
-//       var currentName = nameList[0];
-//       var polymerList = encodedDB.MONOMER_DB.PolymerList.Polymer;
-//       for(i=0; i < polymerList.length; i++){
-//         if(polymerList[i]._polymerType.match(currentName)){
-//           while(nameList.length > 1){
-//             nameList.shift();
-//             var monomerList = polymerList[i].Monomer;
-//             for(j=0; j < monomerList.length; j++){
-//               if(monomerList[i].MonomerID.match(nameList[]))
-//             }
-//           }
-//         }
-//       }
-//     };
-// */
-/*    function getCategorizedMonomer(polymerId, groupId, name) {
-      var groupGetter = getMonomerGroup(polymerId, groupId);
-      var output = { returnSuccess: false };
-      if(!groupGetter.returnSuccess){
-        return output;
-      }
-      var group = groupGetter.result;
-      for (var monomer in group.Monomer) {
-        if (monomer._name.match(name)) {
-          output = { returnSuccess: true, result: monomer };
-          return output;
-        }
-      }
-      return output;
-    }
 
-    // getPolymers - returns the list of polymers
-    var getPolymers = function () {
-      return categorizedDB.Polymer;
-    };
-
-    // returns the full categorized DB
-//    var getCategorizedDB = function () {
-//      if (!init) {
-//        initLink();
-//      }
-//      return categorizedDB.Template;
-//    };
-     // returns the entire encoded db
-//    var getEncodedDB = function(){
-//      if (!init) {
-//        initLink();
-//      }
-//      return encodedDB.MONOMER_DB;
-//    };
-*/
-//     self.getPolymers = getPolymers;
-//     self.getCategorizedMonomer = getCategorizedMonomer;
     self.sanityCheck = function () {
       return '5';
     };
