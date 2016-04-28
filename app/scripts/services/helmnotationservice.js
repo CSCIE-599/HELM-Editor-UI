6efa8c23077b059eee46db0fa346dce7cb94ba38
Justin Sanford <justin.h.sanford@gmail.com>
2016-04-28 16:51:15 -0700

    updated helm notation service to parse out sequences and connections

--- app/scripts/services/helmnotationservice.js
+++ app/scripts/services/helmnotationservice.js
@@ -13,6 +13,77 @@ angular.module('helmeditor2App')
 
     // store the current HELM string
     var helm = '';
+    var sequences = [];
+    var connections = [];
+
+    // parses a given helm string into the sequences and connections
+    var parseHelm = function (helmString) {
+      // store the whole string
+      helm = helmString;
+      sequences = [];
+      connections = [];
+
+      // if we have something, try to get everything out of it
+      if (helm.length > 0) {
+        // get the sequences
+        var sequenceString = helmString.substring(0, helmString.indexOf('$'));
+        while (sequenceString.indexOf('|') > -1) {
+          var strSequence = sequenceString.substring(0, sequenceString.indexOf('|'));
+          if (strSequence.length > 0) {
+            sequences.push(parseSequenceString(strSequence));
+          }
+          sequenceString = sequenceString.substring(sequenceString.indexOf('|') + 1);
+        }
+        // and the last one
+        if (sequenceString.length > 0) {
+          sequences.push(parseSequenceString(sequenceString));
+        }
+
+        // and get the connections
+        helmString = helmString.substring(helmString.indexOf('$') + 1);
+        var connectionString = helmString.substring(0, helmString.indexOf('$'));
+        while (connectionString.indexOf('|') > -1) {
+          var strConnection = connectionString.substring(0, connectionString.indexOf('|'));
+          if (strConnection.length > 0) {
+            connections.push(parseConnectionString(strConnection));
+          }
+          connectionString = connectionString.substring(connectionString.indexOf('|') + 1);
+        }
+        // and the last one
+        if (connectionString.length > 0) {
+          connections.push(parseConnectionString(connectionString));
+        }
+      }
+    };
+
+    // parses a single sequence string into its parts
+    var parseSequenceString = function (sequenceString) {
+      var sequence = {};
+      sequence.name = sequenceString.substring(0, sequenceString.indexOf('{'));
+      sequence.notation = sequenceString.substring(sequenceString.indexOf('{') + 1 , sequenceString.indexOf('}'));
+      return sequence;
+    };
+
+    // parses a single connection string into its parts
+    var parseConnectionString = function (connectionString) {
+      var connection = {};
+      connection.source = {};
+      connection.dest = {};
+      connection.source.sequenceName = connectionString.substring(0, connectionString.indexOf(','));
+      connectionString = connectionString.substring(connectionString.indexOf(',') + 1);
+      connection.dest.sequenceName = connectionString.substring(0, connectionString.indexOf(','));
+      connectionString = connectionString.substring(connectionString.indexOf(',') + 1);
+      connection.source.attachment = {};
+      connection.source.attachment.nodeNum = connectionString.substring(0, connectionString.indexOf(':'));
+      connectionString = connectionString.substring(connectionString.indexOf(':') + 1);
+      connection.source.attachment.point = connectionString.substring(0, connectionString.indexOf('-'));
+      connectionString = connectionString.substring(connectionString.indexOf('-') + 1);
+      connection.dest.attachment = {};
+      connection.dest.attachment.nodeNum = connectionString.substring(0, connectionString.indexOf(':'));
+      connectionString = connectionString.substring(connectionString.indexOf(':') + 1);
+      connection.dest.attachment.point = connectionString;
+      return connection;
+    };
 
     // retrieve the current HELM string
     var getHelm = function () {
@@ -21,10 +92,28 @@ angular.module('helmeditor2App')
 
     // set the current helm string
     var setHelm = function (helmString) {
-      helm = helmString;
+      parseHelm(helmString);
+    };
+
+    // retrieve the sequences
+    var getSequences = function () {
+      return sequences;
+    };
+
+    // retrieve the connections
+    var getConnections = function () {
+      return connections;
+    };
+
+    // add the new sequence to the current HELM string
+    var addNewSequence = function (sequence) {
+      console.log(sequence);
     };
 
     // make things global
     self.getHelm = getHelm;
     self.setHelm = setHelm;
+    self.getSequences = getSequences;
+    self.getConnections = getConnections;
+    self.addNewSequence = addNewSequence;
   });
--- test/spec/services/helmnotationservice.js
+++ test/spec/services/helmnotationservice.js
@@ -22,4 +22,155 @@ describe('Service: HELMNotationService', function () {
     expect(HELMNotationService.getHelm()).toBe(helmString);
   });
 
+  it('should initialize with blank connections', function () {
+    expect(HELMNotationService.getConnections()).toEqual([]);
+  });
+
+  it('should initialize with blank sequences', function () {
+    expect(HELMNotationService.getSequences()).toEqual([]);
+  });
+
+  it('should be able to parse a single sequence with no connections', function () {
+    var helmString = 'RNA1{P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)}$$$$';
+    HELMNotationService.setHelm(helmString);
+    expect(HELMNotationService.getHelm()).toBe(helmString);
+    var sequences = HELMNotationService.getSequences();
+    expect(sequences.length).toBe(1);
+    expect(sequences[0].name).toBe('RNA1');
+    expect(sequences[0].notation).toBe('P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)');
+  });
+
+  it('should clear the sequences and connections when you clear the helm notation', function () {
+    var helmString = 'RNA1{P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)}$$$$';
+    HELMNotationService.setHelm(helmString);
+    expect(HELMNotationService.getHelm()).toBe(helmString);
+    var sequences = HELMNotationService.getSequences();
+    expect(sequences.length).toBe(1);
+    expect(sequences[0].name).toBe('RNA1');
+    expect(sequences[0].notation).toBe('P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)');
+    HELMNotationService.setHelm('');
+    expect(HELMNotationService.getSequences().length).toBe(0);
+    expect(HELMNotationService.getConnections().length).toBe(0);
+  });
+
+  it('should be able to parse a notation with multiple sequences in it', function () {
+    var helmString = 'RNA1{P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)}|CHEM1{SS3}|PEPTIDE1{A.R.C.A.A.K.T.C.D.A}$$$$';
+    HELMNotationService.setHelm(helmString);
+    expect(HELMNotationService.getHelm()).toBe(helmString);
+    var sequences = HELMNotationService.getSequences();
+    expect(sequences.length).toBe(3);
+    expect(sequences[0].name).toBe('RNA1');
+    expect(sequences[0].notation).toBe('P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)');
+    expect(sequences[1].name).toBe('CHEM1');
+    expect(sequences[1].notation).toBe('SS3');
+    expect(sequences[2].name).toBe('PEPTIDE1');
+    expect(sequences[2].notation).toBe('A.R.C.A.A.K.T.C.D.A');
+  });
+
+  it('should be able to parse a notation with no connections', function () {  
+    var helmString = 'RNA1{P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)}$$$$';
+    HELMNotationService.setHelm(helmString);
+    expect(HELMNotationService.getHelm()).toBe(helmString);
+    var sequences = HELMNotationService.getSequences();
+    expect(sequences.length).toBe(1);
+    expect(sequences[0].name).toBe('RNA1');
+    expect(sequences[0].notation).toBe('P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)');
+    var connections = HELMNotationService.getConnections();
+    expect(connections.length).toBe(0);
+  });
+
+  it('should be able to parse a notation with one connection', function () {
+    var helmString = 'RNA1{P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)}|CHEM2{SS3}$RNA1,CHEM2,5:R3-1:R1$$$';
+    HELMNotationService.setHelm(helmString);
+    expect(HELMNotationService.getHelm()).toBe(helmString);
+    var sequences = HELMNotationService.getSequences();
+    expect(sequences.length).toBe(2);
+    expect(sequences[0].name).toBe('RNA1');
+    expect(sequences[0].notation).toBe('P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)');
+    expect(sequences[1].name).toBe('CHEM2');
+    expect(sequences[1].notation).toBe('SS3');
+    var connections = HELMNotationService.getConnections();
+    expect(connections.length).toBe(1);
+    expect(connections[0]).toEqual({
+      source: {
+        sequenceName: 'RNA1',
+        attachment: {
+          nodeNum: '5',
+          point: 'R3'
+        }
+      },
+      dest: {
+        sequenceName: 'CHEM2',
+        attachment: {
+          nodeNum: '1',
+          point: 'R1'
+        }
+      }
+    });
+  });
+
+  it('should be able to parse a notation with multiple connections', function () {
+    var helmString = 'RNA1{P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)}|CHEM2{SS3}$RNA1,CHEM2,5:R3-1:R1|RNA1,CHEM2,2:R2-2:R3|CHEM2,CHEM2,1:R2-6:R3$$$';
+    HELMNotationService.setHelm(helmString);
+    expect(HELMNotationService.getHelm()).toBe(helmString);
+    var sequences = HELMNotationService.getSequences();
+    expect(sequences.length).toBe(2);
+    expect(sequences[0].name).toBe('RNA1');
+    expect(sequences[0].notation).toBe('P.R(A)P.R(G)P.R(C)P.R(U)P.R(T)P.R(T)P.R(T)P.R(T)');
+    expect(sequences[1].name).toBe('CHEM2');
+    expect(sequences[1].notation).toBe('SS3');
+    var connections = HELMNotationService.getConnections();
+    expect(connections.length).toBe(3);
+    expect(connections[0]).toEqual({
+      source: {
+        sequenceName: 'RNA1',
+        attachment: {
+          nodeNum: '5',
+          point: 'R3'
+        }
+      },
+      dest: {
+        sequenceName: 'CHEM2',
+        attachment: {
+          nodeNum: '1',
+          point: 'R1'
+        }
+      }
+    });
+
+    expect(connections[1]).toEqual({
+      source: {
+        sequenceName: 'RNA1',
+        attachment: {
+          nodeNum: '2',
+          point: 'R2'
+        }
+      },
+      dest: {
+        sequenceName: 'CHEM2',
+        attachment: {
+          nodeNum: '2',
+          point: 'R3'
+        }
+      }
+    });
+
+    expect(connections[2]).toEqual({
+      source: {
+        sequenceName: 'CHEM2',
+        attachment: {
+          nodeNum: '1',
+          point: 'R2'
+        }
+      },
+      dest: {
+        sequenceName: 'CHEM2',
+        attachment: {
+          nodeNum: '6',
+          point: 'R3'
+        }
+      }
+    });
+  });
+
 });
