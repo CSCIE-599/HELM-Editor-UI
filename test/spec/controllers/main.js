34317c8fb05d5c49142ae34abb9225b459f6d62a
tgirija <thankam@gmail.com>
2016-05-04 22:24:46 -0400

    unit tests added and clean up

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -26,18 +26,6 @@ describe('Controller: MainCtrl', function () {
     expect(MainCtrl.polyTypes[2].value).toBe('PEPTIDE');
   });
 
- it('should invoke factory to get HELM notation', function () {
-
- });
-
- it('should invoke factory to validate the HELM notation', function () {
-
- });
-
- it('should invoke factory to get molecular weight', function () {
-
- });
-
 //It should draw graphical image on the canvas
 it('should parse the sequence, and generate the graph', function () {    
   var notation = 'RNA1{R(A)P.[mR](U)[sP].R(G)P.R([5meC])P.[dR](T)P.[dR](T)}$$$$';
@@ -81,32 +69,36 @@ it('should draw Peptide sequences', function () {
    expect(angular.equals(subGraph.last.name, 'Aca')).toBe(true);
 });
 
+
+it('should draw a linear graph', function () {
+    var pos = {x:200, y:70};
+    var sequenceArr=[];
+    var sequenceObj = { name:'PEPTIDE1', sequence: ['A','E','E']};
+    sequenceArr.push(sequenceObj);
+    var destObj = {nodeID:0, name:''};
+    var sourceObj = destObj;
+    var connObj = {source: sourceObj, dest:destObj};
+    var subGraph = scope.makeLinearGraph(sequenceObj.sequence, 'forward', 'PEPTIDE', pos,  sequenceObj.name, [connObj], sequenceArr);
+    expect(angular.equals(subGraph.last.name, 'E')).toBe(true);
+    expect(angular.equals(subGraph.first.name, 'A')).toBe(true);
+    expect(angular.equals(subGraph.nodes.length, 3)).toBe(true);
+});
+
+it('should draw a cyclical graph', function () {
+    var pos = {x:200, y:70};
+    var subGraph = scope.makeCyclicalGraph(['R','Y','F','L','W','V','F','P','L'], 'PEPTIDE', pos,  'forward');
+    expect(angular.equals(subGraph.last.name, 'L')).toBe(true);
+    expect(angular.equals(subGraph.first.name, 'R')).toBe(true);
+    expect(angular.equals(subGraph.nodes.length, 9)).toBe(true);
+});
+
+
 it('should identify a cyclical sequence', function () {   
    var sourceObj = {nodeID:1, name:'PEPTIDE1'};
    var destObj = {nodeID:4, name:'PEPTIDE1'};   
    var connObj = {source: sourceObj, dest:destObj};
    expect(scope.isCyclical('PEPTIDE1', [connObj])).toBe(true);
 });
-
-it('should make the connections requested in HELM Notation', function () {
- 
-   
-});
-
-it('should return source node id and dest node id for cyclical connections', function () {
- 
-
-  
-});
-
-it('should slice a given sequence into cycles and linear sub-sequences', function () {
- 
-
-  
-});
-
-
-//function returns source node id and dest node id for cyclical connections
 
 it('should create a new connection and add to the view', function () {
   var node1 =  scope.addNewNode('G', 'PEPTIDE', '#00C3FF', true, 40 , 40);
@@ -131,10 +123,5 @@ it('should reset canvas', function () {
   
 });
 
-it('should convert from titles to types', function () {
- 
- 
-  
-});
 
 });
