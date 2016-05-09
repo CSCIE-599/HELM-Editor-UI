7070786e3a1acf77b76b7fcf87cf86364c7e0551
Justin Sanford <justin.h.sanford@gmail.com>
2016-05-09 17:07:05 -0500

    add monomer click and double click tests

--- test/e2e/scenarios/monomerlibrary.js
+++ test/e2e/scenarios/monomerlibrary.js
@@ -129,4 +129,71 @@ describe('monomer library', function () {
     expect(exploreViewDisplay.all(by.css('.category-holder')).get(4).all(by.css('.monomer-directive')).get(0).element(by.tagName('span')).getText()).toBe('sDBL');
   });
 
+  it('should be able to click on a monomer to select it', function () {
+    var monomerLibraryHolder = element(by.css('.library-column'));
+    var searchInput = monomerLibraryHolder.element(by.css('.search-input'));
+
+    // make sure the display shows up for RNA
+    var searchViewDisplay = element(by.id('searchViewDisplay'));
+    searchInput.sendKeys('as');
+    expect(searchViewDisplay.all(by.css('.monomer-directive')).count()).toBe(1);
+    expect(searchViewDisplay.all(by.css('.monomer-directive')).get(0).element(by.tagName('span')).getText()).toBe('nasP');
+    searchViewDisplay.element(by.css('.monomer-directive')).click();
+    
+    // make sure it's selected
+    expect(searchViewDisplay.element(by.css('.monomer-directive')).element(by.tagName('span')).getAttribute('class')).toContain('monomer-selected');
+
+    // click it again to unselect it
+    searchViewDisplay.element(by.css('.monomer-directive')).click();
+    
+    // make sure it's unselected
+    expect(searchViewDisplay.element(by.css('.monomer-directive')).element(by.tagName('span')).getAttribute('class')).not.toContain('monomer-selected');    
+  });
+  
+  it('should be able to double click on a monomer to get the details when it is not a fragment', function () {
+    var monomerLibraryHolder = element(by.css('.library-column'));
+    var searchInput = monomerLibraryHolder.element(by.css('.search-input'));
+
+    // make sure the display shows up for RNA
+    var searchViewDisplay = element(by.id('searchViewDisplay'));
+    searchInput.sendKeys('as');
+    expect(searchViewDisplay.all(by.css('.monomer-directive')).get(0).element(by.tagName('span')).getText()).toBe('nasP');
+    browser.actions().doubleClick(searchViewDisplay.element(by.css('.monomer-directive')).element(by.tagName('span'))).perform();
+    browser.sleep(1500);
+    
+    // find the modal body
+    var modalBody = element(by.css('.modal-body'));
+    var table = modalBody.element(by.css('.table-full'));
+    var rows = table.all(by.tagName('tr'));
+    var idRow = rows.get(0);
+    var typeRow = rows.get(5);
+    expect(idRow.element(by.css('.table-label')).getText()).toBe('ID');
+    expect(idRow.element(by.css('.table-data')).getText()).toBe('nasP');
+    expect(typeRow.element(by.css('.table-label')).getText()).toBe('Monomer Type');
+    expect(typeRow.element(by.css('.table-data')).getText()).toBe('Backbone');
+  });
+
+  it('should be able to double click on a monomer to get the details when it is a fragment', function () {
+    var monomerLibraryHolder = element(by.css('.library-column'));
+    var searchInput = monomerLibraryHolder.element(by.css('.search-input'));
+
+    // make sure the display shows up for RNA
+    var searchViewDisplay = element(by.id('searchViewDisplay'));
+    searchInput.sendKeys('a');
+    expect(searchViewDisplay.all(by.css('.monomer-directive')).get(0).element(by.tagName('span')).getText()).toBe('A');
+    browser.actions().doubleClick(searchViewDisplay.all(by.css('.monomer-directive')).get(0).element(by.tagName('span'))).perform();
+    browser.sleep(1500);
+    
+    // find the modal body
+    var modalBody = element(by.css('.modal-body'));
+    var table = modalBody.element(by.css('.table-full'));
+    var rows = table.all(by.tagName('tr'));
+    var idRow = rows.get(0);
+    var notationRow = rows.get(2);
+    expect(idRow.element(by.css('.table-label')).getText()).toBe('ID');
+    expect(idRow.element(by.css('.table-data')).getText()).toBe('A');
+    expect(notationRow.element(by.css('.table-label')).getText()).toBe('Fragment Notation');
+    expect(notationRow.element(by.css('.table-data')).getText()).toBe('R(A)P');
+  });
+
 });
