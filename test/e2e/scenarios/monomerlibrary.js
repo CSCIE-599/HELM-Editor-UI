1d721a9c469795b24f9595b82acf054fc895055f
Justin Sanford <justin.h.sanford@gmail.com>
2016-05-08 20:49:02 -0500

    framework for monomerlibrary e2e tests

---
+++ test/e2e/scenarios/monomerlibrary.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('monomer library', function () {
+  // start at the main page
+  beforeEach(function () {
+    browser.get('/');
+  });
+
+  it('should display the placeholder immediately upon loading', function () {
+    var monomerLibraryHolder = element(by.css('.library-column'));
+    expect(monomerLibraryHolder.element(by.tagName('div')).get(1).isDisplayed()).toBeTruthy();
+  });
+
+  // it('should display all three main types', function () {
+  //   var monomerLibraryHolder = element(by.css('.library-column'));
+
+  // });
+
+});
